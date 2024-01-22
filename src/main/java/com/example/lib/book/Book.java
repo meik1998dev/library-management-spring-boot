6811520f5f6384c25f17e3286d35a1fabaed8910
meik1998news <mohammad+test@newsifier.com>
2024-01-22 23:49:18 +0300

    Add getters and setters to Book entity & change id type to Long

--- src/main/java/com/example/lib/book/Book.java
+++ src/main/java/com/example/lib/book/Book.java
@@ -10,13 +10,18 @@ import java.util.List;
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  String id;
-    private  String title;
-    private  String author;
-    private  int publicationYear;
-    private  String ISBN;
+    private Long id;
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String ISBN;
 
-    public Book(String id, String title, String author, int publicationYear, String ISBN) {
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BorrowingRecord> borrowingRecords;
+
+    public Book() {}
+
+    public Book(Long id, String title, String author, int publicationYear, String ISBN) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -24,15 +29,63 @@ public class Book {
         this.ISBN = ISBN;
     }
 
-
-    @OneToMany(mappedBy = "book")
-    private List<BorrowingRecord> borrowingRecords;
-
-    public Book() {
-
+    // Getters and Setters
+    public Long getId() {
+        return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public List<BorrowingRecord> getBorrowingRecords() {
+        return borrowingRecords;
+    }
+
+    public void setBorrowingRecords(List<BorrowingRecord> borrowingRecords) {
+        this.borrowingRecords = borrowingRecords;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", ISBN='" + ISBN + '\'' +
+                '}';
     }
 }
