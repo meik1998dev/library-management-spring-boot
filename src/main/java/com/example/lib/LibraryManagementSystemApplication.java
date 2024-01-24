45bfc96abca96e45fb66004bc6ae2077f0577a34
meik1998news <mohammad+test@newsifier.com>
2024-01-24 11:53:40 +0300

    Setup caching

--- src/main/java/com/example/lib/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/lib/LibraryManagementSystemApplication.java
@@ -2,9 +2,11 @@ package com.example.lib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableCaching
 @EnableAspectJAutoProxy
 public class LibraryManagementSystemApplication {
     public static void main(String[] args) {
